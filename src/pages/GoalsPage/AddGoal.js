2cf50be5170265dffb23db65e9ebc2732ef982d9
Santiago Mariani <santiagomariani2@gmail.com>
2020-06-25 00:08:56 -0300

    little modification in buttons

--- src/pages/GoalsPage/AddGoal.js
+++ src/pages/GoalsPage/AddGoal.js
@@ -3,7 +3,7 @@ import Button from '@material-ui/core/Button'
 import AddListItem from './AddListItem'
 
 export default function AddGoal ({onClickAddGoal}) {
-  return (<AddListItem buttonComponent={(onClick) => <Button onClick={onClick}>Add a new Goal</Button>}
+  return (<AddListItem buttonComponent={(onClick) => <Button variant="outlined" onClick={onClick}>Add a new Goal</Button>}
                         onClickAdd={onClickAddGoal}
                         dialogTitle={'Add a new Goal'} />)
 }
--- src/pages/GoalsPage/AddGoalType.js
+++ src/pages/GoalsPage/AddGoalType.js
@@ -3,7 +3,7 @@ import Button from '@material-ui/core/Button'
 import AddListItem from './AddListItem'
 
 export default function AddGoalType ({onClickAddGoalType}) {
-  return (<AddListItem buttonComponent={(onClick) => <Button onClick={onClick}>Add a new Goal Type</Button>}
+  return (<AddListItem buttonComponent={(onClick) => <Button variant="outlined" onClick={onClick}>Add a new Goal Type</Button>}
                         onClickAdd={onClickAddGoalType}
                         dialogTitle={'Add a new Goal Type'} />)
 }
