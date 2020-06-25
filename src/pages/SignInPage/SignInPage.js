d87ba387de479927db7b95283cd2ca112d7d2aa9
Santiago Mariani <santiagomariani2@gmail.com>
2020-06-25 02:19:47 -0300

    add loading in sign up and sign in pages

--- src/pages/SignInPage/SignInPage.js
+++ src/pages/SignInPage/SignInPage.js
@@ -12,19 +12,10 @@ import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
 import { useHistory } from 'react-router-dom'
 import Alert from '@material-ui/lab/Alert';
-
-function Copyright () {
-  return (
-    <Typography variant='body2' color='textSecondary' align='center'>
-      {'Copyright © '}
-      <Link color='inherit' href='#'>
-        ToDoList
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  )
-}
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Copyright from 'components/Copyright';
+import { trackPromise} from 'react-promise-tracker';
+import { usePromiseTracker } from "react-promise-tracker";
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -59,6 +50,8 @@ export default function SignInPage (props) {
 
   const history = useHistory()
 
+  const { promiseInProgress } = usePromiseTracker();
+
   const handleChangeUserPassword = (e) => {
     setPassword(e.target.value)
   }
@@ -69,37 +62,39 @@ export default function SignInPage (props) {
 
   const handleClickSignIn = (e) => {
     setNoEmailError(false)
-    fetch('/api/signin', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ email: email, password: password })
-    }).then(res => res.json()).then(data => {
-      if (data['message'] === 'Form field errors') {
-        let errors = data['fields']
-        if (errors['email']) {
-          setNoEmailError(true)
-        } else {
+    trackPromise(
+      fetch('/api/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email: email, password: password })
+      }).then(res => res.json()).then(data => {
+        if (data['message'] === 'Form field errors') {
+          let errors = data['fields']
+          if (errors['email']) {
+            setNoEmailError(true)
+          } else {
+            setNoEmailError(false)
+          }
+          if (errors['password']) {
+            setNoPasswordError(true)
+          } else {
+            setNoPasswordError(false)
+          }
+        } else if (data['message'] === 'Email or password is incorrect') {
+          setShowPwEmailAlert(true)
           setNoEmailError(false)
-        }
-        if (errors['password']) {
-          setNoPasswordError(true)
-        } else {
           setNoPasswordError(false)
+          setEmail('')
+          setPassword('')
+        } else if (data['message'] === 'OK') {
+          localStorage.setItem('usertoken', data.token)
+          props.setToken(data.token)
+          history.push('/goals')
         }
-      } else if (data['message'] === 'Email or password is incorrect') {
-        setShowPwEmailAlert(true)
-        setNoEmailError(false)
-        setNoPasswordError(false)
-        setEmail('')
-        setPassword('')
-      } else if (data['message'] === 'OK') {
-        localStorage.setItem('usertoken', data.token)
-        props.setToken(data.token)
-        history.push('/goals')
-      }
-    })
+      })
+    )
   }
 
   const PwEmailAlert = <Alert 
@@ -109,81 +104,84 @@ export default function SignInPage (props) {
                           </Alert>
 
   return (
-    <Container component='main' maxWidth='xs'>
-      <CssBaseline />
-      <div className={classes.paper}> 
-        <Avatar className={classes.avatar}>
-        <LockOutlinedIcon />
-        </Avatar>
-        <Typography component='h1' variant='h5'>
-            Sign in
-        </Typography>
+    <>
+      {promiseInProgress ? <LinearProgress />: null }
+      <Container component='main' maxWidth='xs'>
+        <CssBaseline />
+        <div className={classes.paper}> 
+          <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+          </Avatar>
+          <Typography component='h1' variant='h5'>
+              Sign in
+          </Typography>
 
-        <form className={classes.form} noValidate>
+          <form className={classes.form} noValidate>
 
-        {showPwEmailAlert ? PwEmailAlert: null}
-        
-          <TextField
-            error={noEmailError}
-            FormHelperTextProps={{ error: noEmailError }}
-            helperText={noEmailError ? 'Must insert the email' : ''}
-            variant='outlined'
-            margin='normal'
-            onChange={handleChangeUserEmail}
-            required
-            fullWidth
-            id='email'
-            value={email}
-            label='Email Address'
-            name='email'
-            autoComplete='email'
-            autoFocus
-          />
-          <TextField
-            error={noPasswordError}
-            FormHelperTextProps={{ error: noPasswordError }}
-            helperText={noPasswordError ? 'Must insert the password' : ''}
-            variant='outlined'
-            margin='normal'
-            onChange={handleChangeUserPassword}
-            required
-            fullWidth
-            name='password'
-            label='Password'
-            type='password'
-            id='password'
-            value={password}
-            autoComplete='current-password'
-          />
-          <Button
-            type='button'
-            onClick={handleClickSignIn}
-            fullWidth
-            variant='contained'
-            color='primary'
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-          <Grid container justify="flex-end">
-            {/* Not implemented yet
-            <Grid item xs>
-              <Link href='#' variant='body2'>
-                Forgot password?
-              </Link>
+          {showPwEmailAlert ? PwEmailAlert: null}
+          
+            <TextField
+              error={noEmailError}
+              FormHelperTextProps={{ error: noEmailError }}
+              helperText={noEmailError ? 'Must insert the email' : ''}
+              variant='outlined'
+              margin='normal'
+              onChange={handleChangeUserEmail}
+              required
+              fullWidth
+              id='email'
+              value={email}
+              label='Email Address'
+              name='email'
+              autoComplete='email'
+              autoFocus
+            />
+            <TextField
+              error={noPasswordError}
+              FormHelperTextProps={{ error: noPasswordError }}
+              helperText={noPasswordError ? 'Must insert the password' : ''}
+              variant='outlined'
+              margin='normal'
+              onChange={handleChangeUserPassword}
+              required
+              fullWidth
+              name='password'
+              label='Password'
+              type='password'
+              id='password'
+              value={password}
+              autoComplete='current-password'
+            />
+            <Button
+              type='button'
+              onClick={handleClickSignIn}
+              fullWidth
+              variant='contained'
+              color='primary'
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            <Grid container justify="flex-end">
+              {/* Not implemented yet
+              <Grid item xs>
+                <Link href='#' variant='body2'>
+                  Forgot password?
+                </Link>
+              </Grid>
+              */}
+              <Grid item>
+                <Link href='/signup' variant='body2'>
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
             </Grid>
-            */}
-            <Grid item>
-              <Link href='/signup' variant='body2'>
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
-    </Container>
+          </form>
+        </div>
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Container>
+    </>
   )
 }
